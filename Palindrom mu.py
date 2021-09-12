7c6d63db57377b64108bfd720f429fd5e1839dd5
Mehmet Emin KETRİZ <89727826+Polisite@users.noreply.github.com>
2021-09-12 22:44:42 +0300

    Mehmet Emin Ketriz

--- Palindrom mu.py
+++ Palindrom mu.py
@@ -2,7 +2,7 @@ print("""*************************************
 Palindrom Sorgulama
 *************************************
 """)
-deger = str(input("Sayı veya Değer giriniz: "))
+deger = str(input("Sayı veya Metin giriniz: "))
 deger_tersi = deger[::-1]
 
 if deger == deger_tersi:
