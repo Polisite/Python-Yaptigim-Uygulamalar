e4e9a3f3bdca5eee282dd0b12e014a6e34fdfed2
Mehmet Emin KETRİZ <89727826+Polisite@users.noreply.github.com>
2021-09-12 13:29:52 +0300

    Mehmet Emin Ketriz

---
+++ Yaş Hesaplama.py
@@ -0,0 +1,8 @@
+print("""******************************************
+Yaş Hesaplama
+******************************************
+""")
+d_t = int(input("Hangi yılda doğdun: "))
+s_z = int(input("Şimdi hangi yıldasın: "))
+yas = s_z - d_t
+print('yaş: ',yas)
